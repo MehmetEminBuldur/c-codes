6cedc65db63b63ac3857afa110b81477ceac7cfc
MehmetEminBuldur <50695488+MehmetEminBuldur@users.noreply.github.com>
2020-02-16 13:46:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace selectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,min=0,temp;
+            Console.WriteLine("please enter a number:");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("please enter " + (i+1) + ". element of array:");               
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i = 0; i < n - 1; i++)
+            {
+                min = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }
+                temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine(" " + arr[i] + " ");
+            }
+        }
+    }
+}
