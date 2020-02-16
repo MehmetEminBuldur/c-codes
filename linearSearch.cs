6c6a988f9ac1dde7ff59e2eb41e7f1ab5ef1fea1
MehmetEminBuldur <50695488+MehmetEminBuldur@users.noreply.github.com>
2020-02-16 19:31:47 +0300

    Add files via upload

---
+++ linearSearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace linearSearchh
+{
+    class linearSearch
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter the length of array:");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                Random num = new Random();
+                arr[i] = num.Next(0, 100);
+            }
+            int j = 0;
+            Console.WriteLine("Please enter a number for search in array:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            while (j < n && arr[j] != x)
+            {
+                j++;
+            }
+            if (j >= n)
+            {
+                Console.WriteLine("there is not such en elment in the array!");
+            }
+            else
+            {
+                Console.WriteLine("Index of searched element: " + j);
+            }
+        }
+    }
+}
